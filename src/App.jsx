549541c647e4171e974fcafcbf9fe63f13dc9f37
CesarQuint <pedrocesarquintero001@gmail.com>
2023-02-08 20:36:15 -0600

    Head Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import Body from "./components/Body"
+import Head from "./components/Head"
+import styles from './styles/index.css'
 function App() {
  
   return (
-    <div className="">
-      Hi
+    <>
+    <div className="container">
+      <Head/>
+      <Body/>
     </div>
+    </>
   )
 }
 
---
+++ src/components/Head.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+    <div className='head-container'>
+        {/*Brightness section */}
+        <div className='bright'>
+        </div>
+        <div className="brightMd">
+        </div>
+        {/*Head Section*/}
+        <section className='head'>
+        </section>
+        <section className='head-shadow'>
+        </section>
+        <section className='headBg'>
+        </section>
+        {/*Head Section*/}
+        {/*Mouth Section*/}
+        <section className='mouth'>
+        </section>
+        <div className='hole'>
+        </div>
+        <div className='holeT'>
+
+        </div>
+        <div className='holeSw'>
+        </div>
+        {/*Mouth Section*/}
+        
+        {/*Shadow Section*/}
+        <section className='bordersbh'>
+        </section>
+        <section className='mouthSw'>
+        </section>
+        {/* Shadow Section*/}
+        {/* Eyes section*/}
+        <section className='eyes-container'>
+            <div className='eyes' id='left'></div>
+            <div className='eyes' id='right'></div>
+        </section>
+    </div>
+    
+    </>
+  )
+}
+
+export default Head
