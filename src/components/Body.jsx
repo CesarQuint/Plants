db790bf6febd4cd7c3a1a99114fa72e86310b9ff
CesarQuint <pedrocesarquintero001@gmail.com>
2023-02-09 22:18:31 -0600

    leaf

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,20 @@ function Body() {
   return (
     <div className='bodyContainer'>
         <div className='pot'></div>
+        {/*Cilinder */}
+        <div className="cillinderBrig"></div>
+        <div className="cillinder"></div>
+        <div className="cillinderBg"></div>
+        {/*Cilinder */}
+        {/*Leaf*/}
+        <div className='leaf-up' >
+        </div>
+        <div className='leafM'>
+        </div>
+        <div className='leaf-down'>
+        </div>
+
+        {/*Leaf*/}
     </div>
   )
 }
